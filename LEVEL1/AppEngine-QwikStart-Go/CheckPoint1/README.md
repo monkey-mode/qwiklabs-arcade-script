c595280347d5803786ea62672290bc43f7eb70c2
Suphachai Phetthamrong <suphachai.p@arise.tech>
2023-07-07 00:43:55 +0700

    Add appengine 2

--- LEVEL1/AppEngine-QwikStart-Go/CheckPoint1/README.md
+++ LEVEL1/AppEngine-QwikStart-Go/CheckPoint1/README.md
@@ -1,3 +1,3 @@
 ```
-
+curl -sSL https://github.com/monkey-mode/qwiklabs-arcade-script/blob/main/LEVEL1/AppEngine-QwikStart-Go/CheckPoint1/script.sh | bash -s [REGION]
 ```
--- LEVEL1/AppEngine-QwikStart-PHP/CheckPoint1/script.sh
+++ LEVEL1/AppEngine-QwikStart-PHP/CheckPoint1/script.sh
@@ -5,11 +5,10 @@ gcloud config set compute/region $1
 gcloud services enable appengine.googleapis.com
 
 # Task 2
-git clone https://github.com/GoogleCloudPlatform/golang-samples.git
-cd golang-samples/appengine/go11x/helloworld
+git clone https://github.com/GoogleCloudPlatform/php-docs-samples.git
+cd php-docs-samples/appengine/standard/helloworld
 
 # Task 3
-sudo apt-get install google-cloud-sdk-app-engine-go
 yes Y | gcloud app deploy --region=$1
 
 # Task 4
